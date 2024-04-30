9919c8941d3d84e7e0576ba422382661ab21d75e
Augustuswxz <3306479406@qq.com>
2024-04-30 22:26:05 +0800

    first commit

--- models/yolo.py
+++ models/yolo.py
@@ -89,7 +89,7 @@ class Model(nn.Module):
         else:  # is *.yaml
             import yaml  # for torch hub
             self.yaml_file = Path(cfg).name
-            with open(cfg) as f:
+            with open(cfg, encoding='utf-8') as f:
                 self.yaml = yaml.safe_load(f)  # model dict
 
         # Define model
